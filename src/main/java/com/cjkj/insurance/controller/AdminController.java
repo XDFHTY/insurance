f180db389493208b53ab376113b65da3f57d7c0c
XDFHTY <a1668281642@gmail.com>
2018-05-18 11:23:00 +0800

    05.18-02

---
+++ src/main/java/com/cjkj/insurance/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.cjkj.insurance.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/admin")
+@Api(tags = "管理端接口")
+public class AdminController {
+}
